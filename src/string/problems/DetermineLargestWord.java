6883cb2aad4a917da89de2a96fb55eef4ac4c866
ferdowsr <annaray647@yahoo.com>
2022-05-17 00:39:07 -0600

    definition

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -9,10 +9,10 @@ public class DetermineLargestWord {
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
-        String s="Human brain is a biological learning machine";
+        String s = "Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
-        //        wordNLength.put(1, "Human");
+        wordNLength.put(1, "Human");
 //        wordNLength.put(2, "brain");
 //        wordNLength.put(3, "is");
 //        wordNLength.put(4, "a");
@@ -22,12 +22,25 @@ public class DetermineLargestWord {
     }
 
 
+    public static Map<Integer, String> findTheLargestWord(String wordGiven) {
+    }
 
-    public static Map<Integer, String> findTheLargestWord(String wordGiven){
-        Map<Integer, String> map = new HashMap<Integer, String>();
-        String st = "";
+    Map<Integer, String> map = new HashMap<Integer, String>();
+    String[] words = str.split(" ");
+
+
+        public String[] getTheLargestWords() {
+            return words;
+        }
+    };
+
+
+
+
+
         //implement
 
-        return map;
-    }
-}
+
+
+
+
